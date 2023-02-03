23b02a1f6022ac94f9409e94e9ca9380d62c78d7
Leobardo Licona Soto <leobardo.licona@elektra.com.mx>
2023-02-03 16:27:46 -0600

    feat - Mobile menu and product detail added

--- main.js
+++ main.js
@@ -1,9 +1,46 @@
-const menuEmail = document.querySelector('.navbar-right')
+const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const shoppingCart = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
+const menu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
-menuEmail.addEventListener("click", toggleInactive)
 
-function toggleInactive() {
-    console.log('Click')
+menuEmail.addEventListener("click",  toggleDesktopMenuInactive)
+shoppingCart.addEventListener('click',toggleShoppingCartInactive)
+menu.addEventListener('click', toggleMenuInactive)
+
+function toggleDesktopMenuInactive () {
+   const isProductDetailClosed = productDetail.classList.contains('inactive')
+    if (!isProductDetailClosed){
+        productDetail.classList.toggle('inactive')
+    } 
     desktopMenu.classList.toggle('inactive')
 }
+
+
+function toggleShoppingCartInactive () {
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    if (!isDesktopMenuClosed){
+        desktopMenu.classList.toggle('inactive')
+    }
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+    if (!isMobileMenuClosed ){
+        mobileMenu.classList.toggle('inactive')
+    }
+    productDetail.classList.toggle('inactive')
+}
+
+
+function toggleMenuInactive () {
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    if (!isDesktopMenuClosed){
+        desktopMenu.classList.toggle('inactive')
+    }
+    const isProductDetailClosed = productDetail.classList.contains('inactive')
+    if (!isProductDetailClosed){
+        productDetail.classList.toggle('inactive')
+    }
+    mobileMenu.classList.toggle('inactive')
+}
+
